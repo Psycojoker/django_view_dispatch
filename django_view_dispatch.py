66dbe41f3077eabbbd609ef42da09e010104620a
Laurent Peuch <cortex@worlddomination.be>
2015-04-07 07:06:48 +0200

    [mod] refactor: lambda -> closure

--- django_view_dispatch.py
+++ django_view_dispatch.py
@@ -1,4 +1,6 @@
 def dispatch(**verbs):
-    default = verbs.get("default", "GET")
+    def dispatch_request(request):
+        default = verbs.get("default", "GET")
+        return verbs.get(default, verbs[default.lower()])(request)
 
-    return lambda x: verbs.get(default, verbs[default.lower()])(x)
+    return dispatch_request
