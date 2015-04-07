90304652298534d15923820396cb2aa59a7d69c2
Laurent Peuch <cortex@worlddomination.be>
2015-04-07 07:33:59 +0200

    [enh] use weirdict to support case insensitives verbs

--- django_view_dispatch.py
+++ django_view_dispatch.py
@@ -1,5 +1,11 @@
-def dispatch(**verbs):
+from weirdict.examples import CaseInsensitiveDict
+
+
+def dispatch(**kwargs):
+    verbs = CaseInsensitiveDict()
+    verbs.update(kwargs)
+
     def dispatch_request(request):
-        return verbs.get("get", verbs.get("GET"))(request)
+        return verbs["get"](request)
 
     return dispatch_request
